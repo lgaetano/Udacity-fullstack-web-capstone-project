6916cb292dd5e5d405909da564f25b8396360bc3
Lia Gaetano <lgaetano19@gmail.com>
2021-06-07 16:18:18 -0700

    Update with Flask env variables

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
-export AUTH0_DOMAIN="some_value.auth0.com"
-export API_AUDIENCE="orders"
+export AUTH0_DOMAIN="lgaetano.us.auth0.com"
 export ALGORITHMS=["RS256"]
-export DATABASE_URL="postgres://JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
+export API_AUDIENCE="capstone"
+
+export export DATABASE_URL="postgres:///postgres:1234@localhost:5432/capstone"
+# export DATABASE_URL="postgres:///JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
+
+export FLASK_APP=flaskr
+export FLASK_ENVIRONMENT=debug
