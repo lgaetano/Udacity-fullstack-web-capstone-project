130510a84c9b21d9aae423d905c6aa5e730ffbee
Lia Gaetano <lgaetano19@gmail.com>
2021-06-07 06:56:13 -0700

    Create setup.sh for environment config

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export AUTH0_DOMAIN="some_value.auth0.com"
+export API_AUDIENCE="orders"
+export ALGORITHMS=["RS256"]
+export DATABASE_URL="postgres://JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
