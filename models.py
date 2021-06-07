f5764745ecfba720926d4180b71ef685daf544a8
Lia Gaetano <lgaetano19@gmail.com>
2021-06-07 15:14:52 -0700

    Create model attributes for providers and patients

--- models.py
+++ models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, String, create_engine
+import os
+from sqlalchemy import Column, String, Integer, \
+  ForeignKey, create_engine
+from sqlalchemy.orm import relationship
 from flask_sqlalchemy import SQLAlchemy
 import json
 
@@ -19,26 +22,76 @@ def setup_db(app, database_path=database_path):
 
 
 '''
-Provider: name, insurance type, state, and phone
+Provider: 
+    name, insurance type, state, and phone
 '''
 class Provider(db.Model):  
-  __tablename__ = 'Provider'
+  __tablename__ = 'providers'
 
   id = Column(Integer, primary_key=True)
-  name = Column(String, nullable=False)
-  insurance = Column(String, nullable=False)
-  state = Column(String, nullable=False)
-  phone = Column(String, nullable=False)
+  name = Column(String(120), nullable=False)
+  state = Column(String(120), nullable=False)
+  patients = relationship('patient', backref="provider", lazy=True)
 
-  def __init__(self, name, catchphrase=""):
+  def __init__(self, name, state):
     self.name = name
-    self.catchphrase = catchphrase
+    self.state = state
+    self.patients = patients
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
 
   def format(self):
     return {
       'id': self.id,
       'name': self.name,
-      'insurance': self.insurance,
       'state': self.state,
-      'phone': self.state
-      }
+      'patients': list(map(lambda patient: patient.format(), self.patients))
+    }
+
+'''
+Patient: 
+    name, age, insurance type, and state
+'''
+class Patient(db.Model):  
+  __tablename__ = 'patients'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String(120), nullable=False)
+  age = Column(Integer, nullable=False)
+  state = Column(String(120), nullable=False)
+  provider_id = Column(Integer, ForeignKey('providers.id'), nullable=False)
+
+  def __init__(self, name, age, state, provider_id):
+    self.name = name
+    self.age = age
+    self.state = state
+    self.provider_id = provider_id
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'age': self.age,
+      'state': self.state,
+      'provider_id': self.provider_id
+    }
