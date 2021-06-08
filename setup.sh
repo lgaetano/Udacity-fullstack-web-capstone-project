6100df865b41b2776e6723b04bc5428177fedf4e
Lia Gaetano <lgaetano19@gmail.com>
2021-06-08 14:33:27 -0700

    Update DATABASE_URL

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ export AUTH0_DOMAIN="lgaetano.us.auth0.com"
 export ALGORITHMS=["RS256"]
 export API_AUDIENCE="capstone"
 
-export export DATABASE_URL="postgres:///postgres:1234@localhost:5432/capstone"
+export DATABASE_URL="postgres:///postgres:1234@localhost:5432/capstone"
 # export DATABASE_URL="postgres:///JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
 
 export FLASK_APP=flaskr
