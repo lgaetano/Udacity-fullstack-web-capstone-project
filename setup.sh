60b34dd89577a89847692537023b7f59f9421655
Lia Gaetano <lgaetano19@gmail.com>
2021-06-09 09:13:21 -0700

    Change to app.py root file

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ export API_AUDIENCE="capstone"
 export DATABASE_URL="postgres://postgres@localhost:5432/capstone"
 # export DATABASE_URL="postgres:///JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
 
-export FLASK_APP=flaskr
+export FLASK_APP=app.py
 export FLASK_ENVIRONMENT=debug
