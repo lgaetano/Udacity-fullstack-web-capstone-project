087d7fd4c1fbdaf7967e25739512236afb652668
Lia Gaetano <lgaetano19@gmail.com>
2021-06-09 14:47:13 -0700

    Update DATABASE_URL

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ export ALGORITHMS=["RS256"]
 export API_AUDIENCE="capstone"
 
 export DATABASE_URL="postgres://postgres@localhost:5432/capstone"
+# export DATABASE_URL="postgresql://postgres@localhost:5432/capstone"
 # export DATABASE_URL="postgres:///JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
 
 export FLASK_APP=app.py
