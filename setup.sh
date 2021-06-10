79d1637990af475591c4084488854bc8d931d137
Lia Gaetano <lgaetano19@gmail.com>
2021-06-10 08:16:23 -0700

    Update DATABASE_URL to SQLAlchemy specifications.
    Changed "postgres://" to "postgresql://"

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@ export AUTH0_DOMAIN="lgaetano.us.auth0.com"
 export ALGORITHMS=["RS256"]
 export API_AUDIENCE="capstone"
 
-export DATABASE_URL="postgres://postgres@localhost:5432/capstone"
-# export DATABASE_URL="postgresql://postgres@localhost:5432/capstone"
-# export DATABASE_URL="postgres:///JKLMNOPQ:some_value@ec2-XX-YYY-ZZ-AA.compute-1.amazonaws.com:5432/ABCDEFGHI"
+# export DATABASE_URL="postgres://postgres@localhost:5432/capstone"
+export DATABASE_URL="postgresql://postgres@localhost:5432/capstone"
 
 export FLASK_APP=app.py
 export FLASK_ENVIRONMENT=debug
