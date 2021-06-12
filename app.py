1e1f6a72cc6658a3d93a64ec7d72ee5b89b0a31b
Lia Gaetano <lgaetano19@gmail.com>
2021-06-12 09:43:20 -0700

    Remove unused imports

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-# import os
-# from _typeshed import NoneType
-# from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
-import json
 
 from database.models import setup_db, Provider, Patient
 from auth.auth import AuthError, requires_auth
@@ -48,48 +44,6 @@ def create_app(test_config=None):
 
     except Exception:
       abort(404)
-          
-  @app.route('/patients', methods=['POST'])
-  @requires_auth('post:patients')
-  def add_patient(token):
-    ''' Create new patient record. '''
-    
-    # Get data from body
-    data = request.get_json()
-
-    # If no data, abort
-    if data is None:
-      abort(400, "No data provided")
-
-    name = data.get("name", None)
-    age = data.get("age", None)
-    provider_id = data.get("provider_id", NoneType)
-
-    # If no name supplied, abort
-    if not name:
-        abort(422, "No name provided")
-    # If no age supplied, abort
-    if not age:
-        abort(422, "No age provided")
-    # If no provider_id supplied, abort
-    if not provider_id:
-        abort(422, "No provider_id provided")
-
-    try:
-      #Create new patient record
-      patient = Patient(
-        name=name, 
-        age=age, 
-        provider_id=provider_id)
-      #Insert new patient record
-      patient.insert()
-
-    except Exception:
-      abort(400)
-    
-    return jsonify({
-      "success": True
-    }), 200
 
   @app.route('/providers', methods=['GET'])
   @requires_auth('get:providers')
@@ -109,10 +63,10 @@ def create_app(test_config=None):
     except Exception:
       abort(404)
           
-  @app.route('/providers', methods=['POST'])
-  @requires_auth('post:providers')
-  def add_provider(token):
-    ''' Create new provider record. '''
+  @app.route('/patients', methods=['POST'])
+  @requires_auth('post:patients')
+  def add_patient(token):
+    ''' Create new patient record. '''
     
     # Get data from body
     data = request.get_json()
@@ -121,26 +75,30 @@ def create_app(test_config=None):
     if data is None:
       abort(400, "No data provided")
 
+    # Retrieve data for new record
     name = data.get("name", None)
-    ##TODO: HOW TO ADD PATIENTS
-    # patients = data.get("provider_id", NoneType)
+    age = data.get("age", None)
+    provider_id = data.get("provider_id", None)
 
     # If no name supplied, abort
     if not name:
         abort(422, "No name provided")
-    ##TODO
-    # # If no patient supplied, abort
-    # if not provider_id:
-    #     abort(422, "No provider_id provided")
+    # If no age supplied, abort
+    if not age:
+        abort(422, "No age provided")
+    # If no provider_id supplied, abort
+    if not provider_id:
+        abort(422, "No provider_id provided")
 
     try:
-      #Create new provider record
-      provider = Provider(
-        name=name,
-        # patients=provider_id
-        )
+      #Create new patient record
+      patient = Patient(
+        name=name, 
+        age=age, 
+        provider_id=provider_id)
+
       #Insert new patient record
-      provider.insert()
+      patient.insert()
 
     except Exception:
       abort(400)
@@ -149,7 +107,46 @@ def create_app(test_config=None):
       "success": True
     }), 200
 
+  @app.route('/patients/<int:id>', methods=['PATCH'])
+  @requires_auth('patch:patients')
+  def add_patient(token, id):
+    ''' Update patient record. '''
+    
+    # Query for record
+    patient = Patient.query.filter(Patient.id == id).one_or_none()
 
+    # If no data, abort
+    if patient is None:
+      abort(404, "No patient with id " + {id} + " found.")
+
+    try:
+      # Get data from body
+      data = request.get_json()
+
+      # If no data, abort
+      if data is None:
+        abort(400, "No data provided")
+
+      # Retrieve data to update  record
+      name = data.get("name", None)
+      age = data.get("age", None)
+      provider_id = data.get("provider_id", None)
+
+      # Insert updated data
+      patient.name = name
+      patient.age = age
+      patient.provider_id = provider_id
+
+      # Update patient record
+      patient.update()
+
+    except Exception:
+      abort(422, "Updated failed.")
+    
+    return jsonify({
+      "success": True
+    }), 200
+          
   @app.route('/patients/<patient:patient_id>', methods=['DELETE'])
   @requires_auth('delete:patients')
   def delete_patient(token, patient_id):
